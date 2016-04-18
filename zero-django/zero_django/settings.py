ebde1cd51241bf269848ac3101abdedcf190b3d7
Steven Luscher <steveluscher@fb.com>
2016-04-18 07:46:51 -0700

    Added GraphQL/GraphiQL scaffolding to the Django implementation

--- zero-django/zero_django/settings.py
+++ zero-django/zero_django/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'people',
+    'django_graphiql',
+    'graphene.contrib.django',
 ]
 
 MIDDLEWARE_CLASSES = [
--- zero-django/zero_django/urls.py
+++ zero-django/zero_django/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 from graphene.contrib.django.views import GraphQLView
 
+from .schema import schema
+
 urlpatterns = [
+    url(r'^graphiql', include('django_graphiql.urls')),
+    url(r'^graphql', csrf_exempt(GraphQLView.as_view(schema=schema))),
     url(r'^', include('people.urls')),
 ]
